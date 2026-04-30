f9cc7d85a11fc220b2fdedd88ed42a68ec5da349
Shiv Kumar Singh <shivkumarsingh2501@gmail.com>
2026-04-30 21:08:07 +0530

    variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,22 @@
+const accountId=  123434
+let accountEmail ="shivkumarsingh@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur";
+
+accountEmail ="hc@hc.com"
+accountPassword = "12321433214132"
+accountCity = "Bengaluru"
+let accountState;
+
+//accountId = 2
+console.log(accountId)
+console.table([accountId, accountPassword,accountCity,accountState])
+/*
+
+Prefer not to user var 
+Because of issue in block scope and functional scope 
+*/
+{
+
+
+}
